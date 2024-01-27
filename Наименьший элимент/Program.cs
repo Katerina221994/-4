af63a2cee78a983a4b592aa08d6946ab9cfdfb32
Ekaterina <rfvb.foto@mail.ru>
2024-01-27 09:35:22 +0200

    Добавьте файлы проекта.

---
+++ Наименьший элимент/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Наименьший_элимент
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину последовательности: ");
+            
+            int order = int.Parse(Console.ReadLine());
+
+            int min = int.MaxValue;
+
+            int i = 1;
+
+            while (i <= order)
+            {
+                Console.WriteLine("Введите целое число");
+                
+                int number = int.Parse(Console.ReadLine());
+                
+                if (number < min)
+                {
+                    min = number;
+                    
+                }
+                i++;
+                
+
+            }
+            Console.WriteLine("Минимальное число: " + min);
+            Console.ReadLine();
+        }
+    }
+}
